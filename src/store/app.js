3b79c2e7b943e682dd3b86aa6ff1c843002d130b
river25846 <river8424@gmail.com>
2023-05-17 20:02:17 +0800

    Bot架構建置appStore(Discord.js)

--- src/store/app.js
+++ src/store/app.js
@@ -2,7 +2,8 @@ import {defineStore} from 'pinia'
 
 export const useAppStore = defineStore('app', {
     state: () => ({
-
+        client:null,
+        commandsActionMap:null
      }),
     getters: {
       
